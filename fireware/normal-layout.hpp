e7d76fa3a623d0ae4f3bb215e835817ebd4a11bb
YJ <jun.yang@target-energysolutions.com>
2023-09-27 22:17:49 +0800

    fix layout

--- fireware/normal-layout.hpp
+++ fireware/normal-layout.hpp
@@ -7,13 +7,13 @@ KeyPosition fn(4, 1);
 unsigned char keys[][14] = {
     {'`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', KEY_BACKSPACE},
     {KEY_TAB, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'},
-    {KEY_ESC,'a','s','d','f','g','h','j','k','l',':','\'',KEY_RETURN,0},
+    {KEY_ESC,'a','s','d','f','g','h','j','k','l',';','\'',KEY_RETURN,0},
     {KEY_LEFT_SHIFT,'z','x','c','v','b','n','m',',','.','/',0,KEY_RIGHT_SHIFT,0},
     {KEY_LEFT_CTRL,0/*fn*/,KEY_LEFT_GUI,0,' ',0,0,0,KEY_RIGHT_ALT,KEY_DOWN_ARROW,KEY_RIGHT_ARROW,KEY_RIGHT_CTRL,0}};
 unsigned char fn_keys[][14] = {
     {'`', KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12, KEY_BACKSPACE},
     {KEY_TAB, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'},
-    {KEY_ESC,'a','s','d','f','g',KEY_LEFT_ARROW,KEY_DOWN_ARROW,KEY_UP_ARROW,KEY_RIGHT_ARROW,':','\'',KEY_RETURN,0},
+    {KEY_ESC,'a','s','d','f','g',KEY_LEFT_ARROW,KEY_DOWN_ARROW,KEY_UP_ARROW,KEY_RIGHT_ARROW,';','\'',KEY_RETURN,0},
     {KEY_LEFT_SHIFT,'z','x','c','v','b','n','m',',','.','/',0,KEY_RIGHT_SHIFT,0},
     {KEY_LEFT_CTRL,0/*fn*/,KEY_LEFT_GUI,0,' ',0,0,0,KEY_RIGHT_ALT,KEY_DOWN_ARROW,KEY_RIGHT_ARROW,KEY_RIGHT_CTRL,0}};
 
